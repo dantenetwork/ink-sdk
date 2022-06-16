e14f2b11f84273f1b4520b42494fb843bf9c2992
virgil2019 <danten0402@gmail.com>
2022-06-16 14:12:12 +0800

    Ink SDK

--- examples/greeting/lib.rs
+++ examples/greeting/lib.rs
@@ -4,7 +4,10 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod greeting {
-    use ink_sdk::CrossChainBase;
+    use ink_sdk::{
+        CrossChainBase,
+        MultiDestContracts,
+    };
     use ink_prelude::string::String;
     use ink_prelude::vec::Vec;
     use payload::message_define::{
@@ -18,27 +21,54 @@ mod greeting {
         MsgType,
         MessagePayload,
     };
+    use ink_storage::{
+        Mapping,
+        traits::SpreadAllocate,
+    };
+    
+    #[derive(::scale::Encode, ::scale::Decode, Debug, PartialEq, Eq, Copy, Clone)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        MethodNotRegisterd,
+    }
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
+    #[derive(SpreadAllocate)]
     pub struct Greeting {
         cross_chain_contract: Option<AccountId>,
         ret: Option<String>,
+        dest_contract_map: Mapping<String, (ink_prelude::vec::Vec<u8>, String)>,
     }
 
-    /// We use `CrossChainBase` of SDK here
+    /// We use `CrossChainBase` of SDK here, to be able to use the basic cross-chain functionalities.
     impl CrossChainBase for Greeting {
+        fn get_cross_chain_contract_address(& self) -> AccountId {
+            self.cross_chain_contract.unwrap()
+        }
+    }
+
+    /// We use `MultiDestContracts` of SDK here, to be able to send messages to multi chains.
+    impl MultiDestContracts for Greeting {      
+        #[ink(message)]  
+        fn get_dest_contract_info(& self, chain_name: String) -> Option<(ink_prelude::vec::Vec<u8>, String)> {
+            self.dest_contract_map.get(chain_name)
+        }
+
+        #[ink(message)]
+        fn register_dest_contract(&mut self, chain_name: String, contract: String, action: String) {
+            let contract_bytes = Vec::<u8>::from(contract);
+            self.dest_contract_map.insert(chain_name, &(contract_bytes, action));
+        }
     }
 
     impl Greeting {
         #[ink(constructor)]
         pub fn new() -> Self {
-            Self {
-                cross_chain_contract: None,
-                ret: None,
-            }
+            ink_lang::utils::initialize_contract(|_| {
+            })
         }
 
         /// Sets cross-chain contract address
@@ -49,9 +79,10 @@ mod greeting {
 
         /// Sends greeting to another chain 
         #[ink(message)]
-        pub fn send_greeting(&mut self, chain_name: String, greeting: Vec<String>) {
-            let contract = String::try_from("0xa6666D8299333391B2F5ae337b7c6A82fa51Bc9b").unwrap();
-            let action = String::try_from("receiveGreeting").unwrap();
+        pub fn send_greeting(&mut self, chain_name: String, greeting: Vec<String>) -> Result<(), Error> {
+            let dest = self.get_dest_contract_info(chain_name.clone()).ok_or(Error::MethodNotRegisterd)?;
+            let contract = dest.0;
+            let action = dest.1;
 
             let mut msg_payload = MessagePayload::new();
             msg_payload.push_item(String::try_from("greeting").unwrap(), MsgType::InkStringArray, greeting.clone());
@@ -61,9 +92,11 @@ mod greeting {
             sqos.push(ISQoS::new(ISQoSType::Reveal, None));
             let session = ISession::new(0, 0);
             let content = IContent::new(contract, action, data);
-            let message = ISentMessage::new(chain_name.clone(), sqos, content, session);
+            let message = ISentMessage::new(chain_name, sqos, content, session);
 
             self.send_message(message);
+
+            Ok(())
         }
 
         /// Receives greeting from another chain 
